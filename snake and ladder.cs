676e5e60a41724a54142a9b136def2360b5ea89f
sahanab2000 <85185990+sahanab2000@users.noreply.github.com>
2022-10-14 10:05:59 +0530

    Add files via upload

---
+++ snake and ladder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+
+
+namespace SnakeAndLadder
+
+{
+
+
+    internal class StartPosition
+    {
+
+        public static void StartGame()
+        {
+
+            int position = 0;
+
+            Random random = new Random();
+
+            Console.WriteLine(" Player starts at position: " + position);
+
+         }
+    }
+}
